a5b1ece73137c6ae272932daba4c7534a0218a16
Masa Ogata <masayasu.ogata@gmail.com>
2021-04-15 01:12:41 +0900

    Renew javascript for bootstrap's progress

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,27 +1,29 @@
 $(window).on('load', function(){
-  $('.jsdateupdate').each(function(i) {
-    updatedate($(this));
+  $('.progress-submission').each(function(i) {
+    update_progress($(this));
   });
 });
 
-function updatedate(elem){
-  var conference = new Date(elem.attr("conference"));
-  var base = new Date(conference.getFullYear()-1, conference.getMonth(), conference.getDate());
+function date_ratio(base, target){
+  var milliseconds = target.getTime() - base.getTime();
+  var days = Math.ceil(milliseconds / (1000 * 3600 * 24));
 
-  var diff = date_diff(base, new Date());
-  elem.children('.darken').css('width',diff + '%');
-
-  var deadline = elem.attr("deadline");
-  if (deadline) {
-    var diff = date_diff(base, new Date(deadline));
-    elem.children('.colored').css('width', diff + '%');
-    elem.children('.dealine').css('width', diff + '%');
-  }
+  if (days <= 0)
+    return 0;
+  else if (days >= 365)
+    return 100;
+  else
+    return Math.ceil(100 * days / 365);
 }
 
-function date_diff(base, target){
-  var diff = Math.ceil((target.getTime() - base.getTime())/(1000 * 3600 * 24));
-  if (diff <= 0) return 0;
-  else if (diff >= 365) return 100;
-  else return Math.ceil(100 * diff / 365);
+function update_progress(elem) {
+  var base = new Date(elem.children('.overlay').attr("base"));
+
+  var one_year_before = new Date(
+    base.getFullYear() - 1, base.getMonth(), base.getDate());
+
+  var ratio = date_ratio(one_year_before, new Date());
+
+  elem.children('.overlay').css('width', ratio + '%');
+  elem.children('.overlay').attr('aria-valuenow', ratio)
 }
